115abb10618a3a1409d5171cf3a7864380fc0371
David William <contatodavidwilliams@gmail.com>
2025-11-09 18:04:07 -0300

    Finalizado exercicio2 sobre area do circulo

--- src/estrutura_sequencial/Exercicio2.java
+++ src/estrutura_sequencial/Exercicio2.java
@@ -1,4 +1,24 @@
 package estrutura_sequencial;
 
+import java.util.Scanner;
+
+/**
+ * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+ * casas decimais conforme exemplos.
+ * Autor: David William
+ *  */
+
 public class Exercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double area,x;
+
+        System.out.print("Digite o raio do circulo: ");
+        x = sc.nextDouble();
+        area = Math.PI * Math.pow(x,2);
+
+        System.out.printf("Area : %.4f",area);
+
+    }
 }
